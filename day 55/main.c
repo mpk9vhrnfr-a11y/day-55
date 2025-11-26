c446709f033f75d12ed2935d293b02cfff76f224
sanaa <Sanaakumar07@gmail.com>
2025-11-26 17:16:15 +0530

    c

--- day 55/main.c
+++ day 55/main.c
@@ -1,15 +1,23 @@
-//
-//  main.c
-//  day 55
-//
-//  Created by Sanaa Kumar on 26/11/25.
-//
-
-#include <stdlib.h>
 #include <stdio.h>
 
-int main(int argc, const char * argv[]) {
-    // insert code here...
-    printf("Hello, World!\n");
-    return EXIT_SUCCESS;
+int main(void) {
+    int n, i, pivot = -1, sum1 = 0, sum2;
+
+    printf("Enter a positive integer: ");
+    scanf("%d", &n);
+
+    sum2 = n * (n + 1) / 2;
+
+    for(i = 1; i <= n; i++) {
+        sum1 += i;
+        if(sum1 == sum2 - sum1 + i) {
+            pivot = i;
+            break;
+        }
+    }
+
+    printf("%d\n", pivot);
+
+    return 0;
 }
+
